9201b2297d7a3ff2fadd7cda40f40a8f1dea2cf0
정찬호_JCH <elvpfhhrm@gmail.com>
2026-01-21 12:39:33 +0900

    도전 기능 구현 완료

--- Source/SpartaUnrealCPP_H6/Private/MovingPlatformSpawner.cpp
+++ Source/SpartaUnrealCPP_H6/Private/MovingPlatformSpawner.cpp
@@ -6,6 +6,7 @@ AMovingPlatformSpawner::AMovingPlatformSpawner()
 	SpawnRate = 0.0f;
 	MaxSpawnCnt = 10;
 	CurrentSpawnCnt = 0;
+
 }
 
 void AMovingPlatformSpawner::BeginPlay()
@@ -17,22 +18,12 @@ void AMovingPlatformSpawner::BeginPlay()
 
 void AMovingPlatformSpawner::SpawnRandomMovingPlatform()
 {
-	TSet<int32> CreatedIdx;
 	for (int i = 0; i < MaxSpawnCnt; i++)
 	{
 		if (FMath::RandRange(0.0f, 100.0f) < SpawnRate)
 		{
-			int Idx = -1;
-			while (Idx == -1)
-			{
-				Idx = FMath::RandRange(0, SpawnLocations.Num());
-				if (CreatedIdx.Contains(Idx))
-				{
-					Idx = -1;
-				}
-			}
 			FTransform Transform;
-			Transform.SetLocation(SpawnLocations[Idx]);
+			Transform.SetLocation(SpawnLocations[i]);
 			Transform.SetRotation(FRotator::ZeroRotator.Quaternion());
 			Transform.SetScale3D(FVector(1));
 
@@ -40,9 +31,9 @@ void AMovingPlatformSpawner::SpawnRandomMovingPlatform()
 
 			if (MovingActor)
 			{
-				FVector NewSpeed(MoveSpeeds[Idx] * FMath::RandRange(0.5f, 2.0f));
+				FVector NewSpeed(MoveSpeeds[i] * FMath::RandRange(0.5f, 2.0f));
 				MovingActor->SetMoveSpeed(NewSpeed);
-				MovingActor->SetMaxRange(MaxRanges[Idx]);
+				MovingActor->SetMaxRange(MaxRanges[i]);
 				MovingActor->FinishSpawning(Transform);
 			}
 		}
